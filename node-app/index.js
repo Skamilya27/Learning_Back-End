12f97acd3d2b27d8b20a36b06aef60ad23e9f057
Suman <sumankamilya27@gmail.com>
2023-07-15 20:38:53 +0530

    modules

--- node-app/index.js
+++ node-app/index.js
@@ -1,7 +1,22 @@
-// const lib = require('./lib.js')
-import {sum, diff} from "./lib.js";
+// // const lib = require('./lib.js')
+// // import {sum, diff} from "./lib.js";
+// const fs = require('fs');
 
-// console.log(lib.sum(4,5), lib.diff(3,6));
-console.log(sum(4,5), diff(3,6));
+// // const txt = fs.readFileSync('demo.txt', 'utf-8');
+// fs.readFile('demo.txt', 'utf-8', (err, txt) => {
+//     console.log(txt);
+// });
+// // console.log(txt);
 
-const a = 5;
+// // console.log(lib.sum(4,5), lib.diff(3,6));
+// // console.log(sum(4,5), diff(3,6));
+
+// // const a = 5;
+
+
+const express = require('express');
+
+console.log('Hello World');
+
+const server = express();
+server.listen(8080);
